b2d65f9ce50a081ea52680d451cd776140fbec2b
jonathans199 <jso1919@gmail.com>
2023-10-11 19:29:18 -0400

    added 2 more coding challenges

---
+++ 01-easy/reverseInt/index.js
@@ -0,0 +1,10 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// remember it has to return a number type not string!!!
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
